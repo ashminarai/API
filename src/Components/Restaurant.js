0d6f0839a77185b8dc4209874475d286fb87ef40
ashmina <raiashmina404@gmail.com>
2021-09-04 11:18:57 +0545

    updated

---
+++ src/Components/Restaurant.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./style.css";
+
+const Restaurant = () => {
+    return (
+        <>
+        <div className="card-containere">
+            <div className="card">
+                <div className="card-body">
+                    <span className="card-number card-circle subtle">1</span>
+                    <span className="Card-author subtle">Breakfast</span>
+                </div>
+
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Restaurant;
---
+++ src/Components/menuApi.js
@@ -0,0 +1,9 @@
+const Menu = [{
+    id=1,
+    image="",
+    name="maggi",
+    category="breakfast",
+    price="20Rs",
+    description:"jesdfkjewfjk"
+},
+];
