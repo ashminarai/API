55ad10bf8d5fdbff211db27cbd5a64dfc4178b06
ashmina <raiashmina404@gmail.com>
2021-09-04 11:37:46 +0545

    updated

--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -9,8 +9,14 @@ const Restaurant = () => {
                 <div className="card-body">
                     <span className="card-number card-circle subtle">1</span>
                     <span className="Card-author subtle">Breakfast</span>
+                    <h2 className="card-title">Maggi</h2>
+                    <span className="card-description subtle">
+                        Only 2 minutes haha
+                    </span>
+                    <div className="card-read">Read</div>
                 </div>
-
+                {/* <img src= {image} alt="images" className="card-media"></img> */}
+                <span className="card-tag subtle">Order now</span>
             </div>
         </div>
         </>
