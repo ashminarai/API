382f3569f120f8456f98753a4abaa0dffd19499c
ashmina <raiashmina404@gmail.com>
2021-09-05 10:26:24 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import MenuCard from './Components/Basics/MenuCard';
+
 
 function App() {
   return (
     <>
-    <MenuCard/>
+  
     </>
   );
 }
--- src/Components/Basics/Restaurant.js
+++ src/Components/Basics/Restaurant.js
@@ -1,14 +1,1 @@
-import React, {useState} from 'react'
 
-
-const Restaurant = () => {
-    const [menuData, setMenuData]= useState(Menu);
-    return (
-        <>
-       <MenuCard menuData = {menuData}/>
-        </>
-    );
-};
-
-
-export default Restaurant;
